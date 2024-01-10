2a7faa59ea7edadcecf2d288decdf57b34f6ac19
Shubham Lal <shubham.lal@sourcefuse.com>
2024-01-10 20:26:29 +0530

    Simple implementation of search-element in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
 import './App.css';
+import './element/styles.css'
+import '@sourceloop/search-element/search-element'
+import { useEffect, useRef } from 'react';
+import ProjectIcon from './images/box.svg';
+import workspaceIcon from './images/workspaceIcon.png';
+import tableIcon from './images/tables.svg';
+import { mockData } from './data';
+
+
+const SearchSourceType  = {
+  Group: 'lists',
+  Workspace: 'workspaces',
+  Board: 'boards',
+  BoardTemplate: 'boardtemplates',
+  WorkspaceTemplate: 'workspacetemplates',
+}
+
+const models = [
+  {
+    name: SearchSourceType.Workspace,
+    displayName: 'Workspaces',
+    imageUrl: workspaceIcon,
+  },
+  {
+    name: SearchSourceType.Board,
+    displayName: 'Projects',
+    imageUrl: ProjectIcon,
+  },
+  {
+    name: 'list',
+    displayName: 'Lists',
+    imageUrl: tableIcon,
+  },
+  {
+    name: SearchSourceType.WorkspaceTemplate,
+    displayName: 'Workspace Templates',
+    imageUrl: workspaceIcon,
+  },
+  {
+    name: SearchSourceType.BoardTemplate,
+    displayName: 'Project Templates',
+    imageUrl: ProjectIcon,
+  },
+];
+
+const config = {
+  displayPropertyName: 'name',
+  models: models,
+  order: [`name ASC`, `description DESC`],
+  hideCategorizeButton: true,
+  placeholder: 'Search Programs, Projects or Dashboards',
+  categorizeResults: true,
+  saveInRecents: true,
+  limit: 4,
+  hideRecentSearch: false,
+  saveInRecentsOnlyOnEnter: false,
+  limitByType: true,
+};
+
+const searchProvider = {searchApiRequestWithPromise:  async ()=> [
+  {
+      "parentid": "56f80882-1d6f-e4a9-ff34-d5a5330ff994",
+      "parenttype": null,
+      "name": "shubham Project",
+      "modifiedon": "2023-09-29T17:57:54.724Z",
+      "id": "efb02da8-c61d-7477-67dc-6fd3956a7231",
+      "source": "boards",
+      "rank": 0.1
+  },
+  {
+      "parentid": null,
+      "parenttype": null,
+      "name": "Shubham Workspace",
+      "modifiedon": "2023-10-04T07:39:29.010Z",
+      "id": "ea867ed2-ee52-4400-95a2-023d0bf2b5fa",
+      "source": "workspaces",
+      "rank": 0.1
+  }
+], recentSearchApiRequestWithPromise: async()=> async ()=> [
+  {
+      "parentid": "56f80882-1d6f-e4a9-ff34-d5a5330ff994",
+      "parenttype": null,
+      "name": "shubham Project",
+      "modifiedon": "2023-09-29T17:57:54.724Z",
+      "id": "efb02da8-c61d-7477-67dc-6fd3956a7231",
+      "source": "boards",
+      "rank": 0.1
+  },
+  {
+      "parentid": null,
+      "parenttype": null,
+      "name": "Shubham Workspace",
+      "modifiedon": "2023-10-04T07:39:29.010Z",
+      "id": "ea867ed2-ee52-4400-95a2-023d0bf2b5fa",
+      "source": "workspaces",
+      "rank": 0.1
+  }
+]};
 
 function App() {
+  const searchElement = useRef(null);
+
+
+  useEffect(() => {
+    if (searchElement.current) {
+      const element = searchElement.current;
+      element.searchProvider = searchProvider;
+      element.config = config;
+      return () => {
+        element.removeEventListener('clicked', ()=>{});
+      };
+    }
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <sourceloop-search-element
+        ref={searchElement}
+      ></sourceloop-search-element>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,128 @@
+export const mockData = [
+    {
+        "parentid": "6bbbff3e-ac9e-5c0c-f49f-eeaac4e39ce3",
+        "parenttype": null,
+        "name": "2nd sheet",
+        "modifiedon": "2023-03-06T11:14:01.588Z",
+        "id": "17a1095f-ea4a-98ca-9b23-3da7f8beb69f",
+        "source": "boards",
+        "rank": 0.1
+    },
+    {
+        "parentid": "599c4f16-d9a5-3dc6-6923-18461ec8827a",
+        "parenttype": null,
+        "name": "A8ME Sprint 1",
+        "modifiedon": "2023-09-19T08:07:16.337Z",
+        "id": "73214581-a816-b5d2-6678-bda271a9cb5c",
+        "source": "groups",
+        "rank": 0.1
+    },
+    {
+        "parentid": "599c4f16-d9a5-3dc6-6923-18461ec8827a",
+        "parenttype": null,
+        "name": "A8ME Sprint 2",
+        "modifiedon": "2023-09-19T08:07:16.339Z",
+        "id": "32773745-d5a5-d3aa-78fb-ca3c60dec5e9",
+        "source": "groups",
+        "rank": 0.1
+    },
+    {
+        "parentid": "599c4f16-d9a5-3dc6-6923-18461ec8827a",
+        "parenttype": null,
+        "name": "A8ME Sprint 3",
+        "modifiedon": "2023-09-19T08:07:16.336Z",
+        "id": "e8b2fad8-1378-1451-7ab0-00a43be7f355",
+        "source": "groups",
+        "rank": 0.1
+    },
+    {
+        "parentid": "599c4f16-d9a5-3dc6-6923-18461ec8827a",
+        "parenttype": null,
+        "name": "A8ME Sprint 4",
+        "modifiedon": "2023-09-19T08:07:16.341Z",
+        "id": "bf024585-05c3-4146-ba68-21d052d7f6bf",
+        "source": "groups",
+        "rank": 0.1
+    },
+    {
+        "parentid": "2e38a645-8307-a972-ee81-3590c12ed959",
+        "parenttype": null,
+        "name": "Abhi's  Project",
+        "modifiedon": "2023-07-12T13:47:34.602Z",
+        "id": "c2ac5452-e41f-7950-93f8-8ed97199ec31",
+        "source": "boards",
+        "rank": 0.1
+    },
+    {
+        "parentid": "11e089fb-ede4-4d19-835e-5b5988d7b0e5",
+        "parenttype": null,
+        "name": "b4ubuild_sample_07",
+        "modifiedon": "2023-05-22T11:17:53.543Z",
+        "id": "015df695-02e2-f1b1-3e90-0df8af64565a",
+        "source": "boards",
+        "rank": 0.1
+    },
+    {
+        "parentid": "ff52418f-29fe-8d9b-e852-8451081d9945",
+        "parenttype": null,
+        "name": "b4ubuild_sample_07",
+        "modifiedon": "2023-04-11T07:34:25.645Z",
+        "id": "038f16b9-e654-d12e-7bab-f082bd0f9bdf",
+        "source": "boards",
+        "rank": 0.1
+    },
+    {
+        "parentid": null,
+        "parenttype": null,
+        "name": "Initial Set",
+        "modifiedon": "2023-05-29T07:57:03.224Z",
+        "id": "8b464c91-16d8-19b7-2186-8181fd4c29ae",
+        "source": "workspaces",
+        "rank": 0.1
+    },
+    {
+        "parentid": null,
+        "parenttype": null,
+        "name": "QA Testing Data Set",
+        "modifiedon": "2023-07-12T04:31:19.371Z",
+        "id": "2e38a645-8307-a972-ee81-3590c12ed959",
+        "source": "workspaces",
+        "rank": 0.1
+    },
+    {
+        "parentid": null,
+        "parenttype": null,
+        "name": "Samsung",
+        "modifiedon": "2023-10-09T11:08:31.170Z",
+        "id": "3f029fd4-ab89-7502-566d-af55d1285280",
+        "source": "workspaces",
+        "rank": 0.1
+    },
+    {
+        "parentid": null,
+        "parenttype": null,
+        "name": "Secound workspace",
+        "modifiedon": "2023-10-06T13:52:52.509Z",
+        "id": "fbc05b87-d1fa-4e2f-6db9-06cf1b1bcb1b",
+        "source": "workspaces",
+        "rank": 0.1
+    },
+    {
+        "parentid": null,
+        "parenttype": "local",
+        "name": "sigma1",
+        "modifiedon": "2023-10-27T09:00:58.631Z",
+        "id": "bb7807b3-f408-f9db-dab2-15b30478f584",
+        "source": "boardtemplates",
+        "rank": 0.1
+    },
+    {
+        "parentid": null,
+        "parenttype": "master",
+        "name": "sigma h",
+        "modifiedon": "2023-10-27T09:01:13.721Z",
+        "id": "214f8430-8544-5c6a-9e2f-cb80e4fb0200",
+        "source": "boardtemplates",
+        "rank": 0.1
+    }
+] 
